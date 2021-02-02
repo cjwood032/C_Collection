c5528722f229ac3203e79df44a034d36154bc6a3
cjwood032 <cjwood032@gmail.com>
2021-02-02 09:29:01 -0500

    added input

--- SampleApp/SampleApp.c
+++ SampleApp/SampleApp.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char str[] = "Chris";
-    printf("Hi, My name is ");
-    printf(str);
+    char str[100];
+    int i;
+    printf("enter a name and age:\n");
+    //char str[] = "Chris";
+    scanf("%s %d",str, &i);
+    printf("Hi, my name is \n %s and I'm %d years old\n", str, i);
     return 0;
 }
