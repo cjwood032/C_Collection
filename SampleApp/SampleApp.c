484c4b6b76eb1a3198926a53830181e5a477c540
cjwood032 <cjwood032@gmail.com>
2021-02-02 09:30:32 -0500

    double

--- SampleApp/SampleApp.c
+++ SampleApp/SampleApp.c
@@ -2,10 +2,10 @@
 int main()
 {
     char str[100];
-    int i;
-    printf("enter a name and age:\n");
+    double i;
+    printf("enter a name and weight:\n");
     //char str[] = "Chris";
-    scanf("%s %d",str, &i);
-    printf("Hi, my name is \n %s and I'm %d years old\n", str, i);
+    scanf("%s %lf",str, &i);
+    printf("Hi, my name is \n %s and I'm %lf \n", str, i);
     return 0;
 }
