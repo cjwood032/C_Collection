634662c33f77dd324a4006ed54c9bdf9ee6fa318
cjwood032 <cjwood032@gmail.com>
2021-02-05 11:54:09 -0500

    more structs

--- structs/structExample.c
+++ structs/structExample.c
@@ -1,4 +1,4 @@
-
+#include <string.h>
 int main()
 {
     struct date 
@@ -24,5 +24,26 @@ struct date
     struct date valentines = {.month = 2, .day = 14};
     struct date yesterday;
     yesterday = (struct date) {2,5,2020};//compound literals
+    struct date myDates[5] = { {1,1,2000}, {9, 12, 1986}, {9, 19, 1980} };
+    struct date myOtherDates[5] = { 1,1,2000, 9, 12, 1986, 9, 19, 1980 };
+
+    struct month 
+    {
+        int days;
+        char name[3];
+    };
+    struct date2 
+    {
+        int day;
+        struct month mon;
+        int year;
+    } anotherday;
+    anotherday.mon.days= 31;
+    strncpy(anotherday.mon.name,"Jan",3);
+    struct month Feb = {28, {'F','E','B'}};
+    struct date *pdate;
+    pdate = &today;
+    (*pdate).day=6;
+    pdate->month=2;
     return 0;
 }
